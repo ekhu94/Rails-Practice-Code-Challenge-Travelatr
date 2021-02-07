be6a55db3d4a3fec1f6cd1d1ebba6705d1e17516
Erik Huang <erikhuangdrums@gmail.com>
2021-02-07 14:15:11 -0800

    everything done except likes button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,34 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update]
+    before_action :set_post, only: [:show, :upvote, :edit, :update]
+
+    def index
+        @posts = Post.all
+    end
 
     def show
+    end
+
+    def new
+        @post = Post.new
+        @bloggers = Blogger.all
+        @destinations = Destination.all
+    end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to post_path(@post)
+        else
+            @bloggers = Blogger.all
+            @destinations = Destination.all
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
     end
 
     private
